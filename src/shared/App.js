1bcce02a651bd9ce2a24226fe46d1475957e91fc
Kimyechan <vlvkcjswo7@naver.com>
2020-06-24 17:25:34 +0900

    main page css 1

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch, Link as RouterLink } from 'react-router-dom';
+
+import { withStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link'
+import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
 import { Home } from 'pages';
 import Exercise from '../pages/Exercise';
 import Community from '../pages/Community';
@@ -7,15 +18,28 @@ import MyPage from '../pages/MyPage';
 import TrainingBot from '../pages/TrainingBot'
 import SignUp from '../pages/SignUp';
 import Login from '../pages/Login';
-import Top from '../components/Top';
+
 import ExercisePurpose from '../components/ExercisePurpose';
 
+
+const styles = (theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+});
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLogined: false
+            isLogined: false,
+            toggle: false
         }
 
         this.changeLoginState = this.changeLoginState.bind(this);
@@ -41,27 +65,43 @@ class App extends Component {
         })
     }
 
+    handleDrawerToggle = () => {
+        this.setState(
+            { toggle: !this.state.toggle }
+        )
+    }
     componentDidMount() {
         this.changeLoginState();
     }
 
     render() {
+        const classes = this.props;
         return (
-            <div>
-                <Link to="/">TRAINING BOT</Link><br></br>
-                {/* {accountBar} */}
-                {this.state.isLogined ? (
-                    <button onClick={this.handleLogout}>Logout</button>
-                ) : (
-                        <div>
-                            <Link to="/signin">Login</Link>
-                            <Link to="/signup">Signup</Link>
-                        </div>
-                    )}
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={this.handleDrawerToggle}>
+                            <MenuIcon />
+                        </IconButton>
+                        <IconButton color="inherit" component={RouterLink} to="/">TRAINING BOT</IconButton>
+                        {this.state.isLogined ? (
+                            <Button color="inherit" onClick={this.handleLogout}>Logout</Button>
+                        ) : (
+                                <div>
+                                    <Button color="inherit" component={RouterLink} to="/signin">Login</Button>
+                                    <Button color="inherit" component={RouterLink} to="/signup">Signup</Button>
+                                </div>
+                            )
+                        }
+                    </Toolbar>
+                </AppBar>
+                <Drawer open={this.state.toggle}>
+                    <MenuItem onClick={this.handleDrawerToggle}>Home</MenuItem>
+                </Drawer>
                 <ol>
-                    <li><Link to="/exercise">exercise</Link></li>
-                    <li><Link to="/community">community</Link></li>
-                    <li><Link to="/myPage">myPage</Link></li>
+                    <li><Link component={RouterLink} to="/exercise">exercise</Link></li>
+                    <li><Link component={RouterLink} to="/community">community</Link></li>
+                    <li><Link component={RouterLink} to="/myPage">myPage</Link></li>
                 </ol>
                 <Route exact path="/" component={Home} />
                 <Switch>
@@ -69,7 +109,6 @@ class App extends Component {
                     <Route path="/exercise/:kind" component={TrainingBot} />
                     <Route path="/exercise" component={Exercise} />
                 </Switch>
-                {/* <Route path="/signin" component={Login}></Route> */}
                 <Route
                     path="/signin"
                     render={props => (
@@ -86,4 +125,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
