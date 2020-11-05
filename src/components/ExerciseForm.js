8971e2ba56e653ad1fad36b01d9f98bc566ee49e
Kimyechan <vlvkcjswo7@naver.com>
2020-11-05 15:42:20 +0900

    Feat: Exercise Design
    add data of explaining Exercise content

--- src/components/ExerciseForm.js
+++ src/components/ExerciseForm.js
@@ -1,20 +1,83 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import "../css/exercise.css";
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Block } from "@material-ui/icons";
+
+// import "../css/exercise.css";
+
+const styles = {
+    root: {
+        maxWidth: 500,
+    },
+    media: {
+        height: 300,
+        width: 500,
+    },
+    content: {
+        width: 450
+    }
+};
 
 class ExerciseForm extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            exerciseContent : ""
+        }
+    }
+
     render() {
+        const { classes } = this.props;
+
         return (
             <div>
-                <img className="img_view" src={"image/" + this.props.name + ".png"}></img>
-                <Link to={"/exercisepurpose/"+this.props.name}>
-                    <button type="button">Ready</button>
-                </Link>
+                <div>
+                    <Grid
+                        container
+                        spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justify="center"
+                    >
+                        <Grid item xs={7}>
+                            <Card className={classes.root}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        className={classes.media}
+                                        image={"image/" + this.props.name + ".png"}
+                                        title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            {this.props.name}
+                                        </Typography>
+                                        <Typography className={classes.content}variant="body2" color="textSecondary" component="p" align="left">
+                                            {this.props.content}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions style={{justifyContent: 'center'}}>
+                                    <Link to={"/exercisepurpose/" + this.props.name}>
+                                        <Button size="medium" variant="outlined" color="primary">Ready</Button>
+                                    </Link>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </div>
+
             </div>
         )
     }
 }
 
-export default ExerciseForm;
+export default withStyles(styles)(ExerciseForm);
--- src/pages/Exercise.js
+++ src/pages/Exercise.js
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
 import ExerciseForm from '../components/ExerciseForm'
 
+import Button from '@material-ui/core/Button';
+
 import "../css/exercise.css";
 
 class Exercise extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            exerciseName : 'Squat'
+            exerciseName : 'Squat',
+            exerciseContent : "to position yourself close to the ground balancing on the front part of your feet with your legs bent under your body"
         }
     }
 
-    handleExerciseName = (e) => {
-        console.log(e.target.value)
+    handleExercise = (e) => {
+        const squat = "to position yourself close to the ground balancing on the front part of your feet with your legs bent under your body";
+        const lunge = "an exercise in which a person in a standing position steps forward into a position in which the front knee is deeply bent while keeping the torso erect and then returns back to the starting position"
+        const pushUp = "an exercise in which a person, keeping a prone position with the hands palms down under the shoulders, the balls of the feet on the ground, and the back straight, pushes the body up and lets it down by an alternate straightening and bending of the arms."
+        const sideLateralRaise = "A shoulder exercise in which the arm is raised and lowered against resistance – such as through use of a dumbbell, cable, or resistance band –, isolating the deltoids."
+        
+        let content = "";
+        if(e.currentTarget.value == "Squat") {
+            content = squat;
+        } else if(e.currentTarget.value == "PushUp") {
+            content = pushUp;
+        } else if(e.currentTarget.value == "sideLateralRaise"){
+            content = sideLateralRaise;
+        } else if(e.currentTarget.value == "lunge"){
+            content = lunge;
+        }
+
         this.setState({
-            exerciseName:e.target.value
+            exerciseContent : content,
+            exerciseName: e.currentTarget.value // e.target -> e.currentTarget button value를 가지고 오지 않는 문제 해결
         })
+
     }
 
-
     render() {
-
         return (
             <div>
                 <div className="exercise_nav">
                     <div className="exercise_innerNav">
-                        <button className="exercise_innerbutton" value="Squat" onClick={this.handleExerciseName}>Squat</button>
+                        {/* <button className="exercise_innerbutton" value="Squat" onClick={this.handleExerciseName}>Squat</button>
                         <button className="exercise_innerbutton" value="PushUp" onClick={this.handleExerciseName}>PushUp</button>
                         <button className="exercise_innerbutton" value="sideLateralRaise" onClick={this.handleExerciseName}>SideLateral</button>
-                        <button className="exercise_innerbutton" value="lunge" onClick={this.handleExerciseName}>Lunge</button>
+                        <button className="exercise_innerbutton" value="lunge" onClick={this.handleExerciseName}>Lunge</button> */}
+                        <Button size="large" variant="outlined" color="primary" className="exercise_innerbutton" value="Squat" onClick={this.handleExercise}>Squat</Button>
+                        <Button size="large" variant="outlined" color="primary" className="exercise_innerbutton" value="PushUp" onClick={this.handleExercise}>PushUp</Button>
+                        <Button size="large" variant="outlined" color="primary" className="exercise_innerbutton" value="sideLateralRaise" onClick={this.handleExercise}>SideLateral</Button>
+                        <Button size="large" variant="outlined" color="primary" className="exercise_innerbutton" value="lunge" onClick={this.handleExercise}>Lunge</Button>
                     </div>
                 </div>
 
                 <div className="exercise_view">
-                    <p>{this.state.exerciseName}</p>
-                    <ExerciseForm name={this.state.exerciseName}></ExerciseForm>
+                    <ExerciseForm name={this.state.exerciseName} content={this.state.exerciseContent}></ExerciseForm>
                 </div>
             </div>
         );
